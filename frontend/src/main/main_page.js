aa7ec5d55dba44bdcf8670ceff794275412419e7
Eric Balfour <egbalfour@gmail.com>
2022-07-05 10:12:23 -0400

    user auth functional

---
+++ frontend/src/main/main_page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+class MainPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Fun Finder</h1>
+                <footer>
+                    Tap and Snack
+                </footer>
+            </div>
+        );
+    }
+}
+export default MainPage;
