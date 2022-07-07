217f961b16f070ca53c8b7908895d814ec4f0234
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-07-06 23:51:09 -0400

    added my map

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,15 +1,27 @@
 import React from 'react';
-class MainPage extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1> Welcome To Fun Finder</h1>
-                <footer>
-                    Tap and Snack
-                    
-                </footer>
-            </div>
-        );
-    }
+import { useLoadScript } from "@react-google-maps/api"
+import Map from "./../map/map"
+import google_maps_key from "../../config/google_maps_key"
+
+const MainPage = (props) => {
+
+
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: google_maps_key,
+        libraries: ["places"]
+        })
+    
+    if (!isLoaded) return <div>Loading...</div>
+
+    return (
+        <div>
+            <h1> Welcome To Fun Finder</h1>
+            <footer>
+                Tap and Snack
+                <Map />
+                <div id="address-details-container"></div>
+            </footer>
+        </div>
+    );
 }
 export default MainPage;
---
+++ frontend/src/components/map/map.jsx
@@ -0,0 +1,89 @@
+/*global google*/
+import React, { useState, useMemo, userCallback, useRef } from "react";
+import {
+    GoogleMap,
+    Marker,
+    DirectionsRenderer,
+    Circle,
+    MarkerClusterer
+} from "@react-google-maps/api"
+// import mapStyles from "./../mapStyles"
+import Search from "./search";
+import "./../../styles/map.css"
+
+export default function Map() {
+
+    const center = useMemo(() => ({ lat: 40.73629, lng: -73.99379 }), []);
+    const options = {
+        // styles: mapStyles,
+        disableDefaultUI: true,
+        zoomControl: true
+    }
+
+    const mapRef = React.useRef();
+  const onMapLoad = React.useCallback(map => { 
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(12);
+    let map = mapRef.current;
+
+
+
+    let request = { 
+      location: { lat, lng },
+      radius: "500",
+      type: ["church"]  
+    };
+
+    let service = new google.maps.places.PlacesService(mapRef.current);
+    service.nearbySearch(request, callback);
+    // service.getDetails(request, callback)
+    // service.textSearch(request, callback);
+
+    function callback(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (let i = 0; i < results.length; i++) {
+          let place = results[i];
+          new google.maps.Marker({
+            position: place.geometry.location,
+            map
+          });
+          // console.log(results[i].name)
+        //   console.log(results[i].vicinity)
+
+        let container_block = document.getElementById( 'address-details-container' );
+        
+        let address_name_to_insert = document.createElement( 'div' );
+        address_name_to_insert.innerHTML = `${results[i].name}` ;
+        container_block.appendChild( address_name_to_insert )
+        address_name_to_insert.id = 'address-name'
+
+        let address_details_to_insert = document.createElement( 'div' );
+        address_details_to_insert.innerHTML = `${results[i].vicinity}` ;
+        container_block.appendChild( address_details_to_insert )
+        address_details_to_insert.id = 'address-details'
+        }
+      }
+    }
+  }, []);
+
+
+    return <div className="container">
+        <div className="controls"><h1>Map</h1></div>
+        <Search panTo={panTo} />
+        <div className="map">
+            <GoogleMap 
+            zoom={10} 
+            center={center} 
+            mapContainerClassName="map-container"
+            options={options}
+            onLoad={onMapLoad}
+            >
+            </GoogleMap>
+        </div>
+        {/* <div id="address-details-container"></div> */}
+    </div >
+}
