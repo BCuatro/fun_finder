06f73f41868bb1f5f89e995d51ce1bf4e5bd9128
David-Allen <davida.asencio@gmail.com>
2022-07-07 12:08:48 -0400

    fixing google api key file

--- config/google_maps_key.js
+++
@@ -1,1 +0,0 @@
-export default 'AIzaSyAenWdJdoewLrrlDoC9depogkqhuSxBJgA';
---
+++ frontend/src/config/keys.js
@@ -0,0 +1,2 @@
+mongoURI: 'mongodb+srv://bcuatro:oJzRGBrPgipqHQTB@cluster0.gmlqmja.mongodb.net/?retryWrites=true&w=majority';
+secretOrKey: "secret12345678901234567890"
