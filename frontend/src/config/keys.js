2035c523f2c7c6e1a5d4e1aa7a22ffccc5f1de38
David-Allen <davida.asencio@gmail.com>
2022-07-07 21:39:45 -0400

    working with heroku bug

--- frontend/src/config/keys.js
+++ frontend/src/config/keys.js
@@ -1,2 +1,2 @@
-MONGO_URI: 'mongodb+srv://bcuatro:oJzRGBrPgipqHQTB@cluster0.gmlqmja.mongodb.net/?retryWrites=true&w=majority';
+mongoURI: 'mongodb+srv://bcuatro:oJzRGBrPgipqHQTB@cluster0.gmlqmja.mongodb.net/?retryWrites=true&w=majority';
 secretOrKey: "secret12345678901234567890"
