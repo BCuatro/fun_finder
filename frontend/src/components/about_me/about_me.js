a4adfaf76769fb67071dd89ad7c21a9595ab79a9
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-07-21 18:50:20 -0400

    fixed different zoom size issue

--- frontend/src/components/about_me/about_me.js
+++ frontend/src/components/about_me/about_me.js
@@ -2,21 +2,19 @@ import React from 'react';
 import "./../../styles/about_me.css";
 
 class AboutMe extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         return (
             <div className="about-me-container">
                 <div className="Alisher">
-                    Alisher
+                    Alisher Podavonov
+                    
                 </div>
                 <div className="Eric">
-                    Eric
+                    Eric Balfour
                 </div>
                 <div className="David-Allen">
-                    David-Allen
+                    David-Allen Asencio
                 </div>
             </div>
         );
