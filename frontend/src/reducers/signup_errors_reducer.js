3063c455648f5d600dc46aeb4eac8acaa6d0a0db
Eric Balfour <egbalfour@gmail.com>
2022-07-21 11:51:56 -0400

    added backend AWS features

--- frontend/src/reducers/signup_errors_reducer.js
+++ frontend/src/reducers/signup_errors_reducer.js
@@ -1,19 +1,19 @@
-const { RECEIVE_SESSION_ERRORS,RECEIVE_CURRENT_USER, RECEIVE_SIGNUP_ERRORS } = require("../actions/session_actions");
-
+const { RECEIVE_SIGNUP_ERRORS,RECEIVE_CURRENT_USER, REMOVE_SIGNUP_ERRORS } = require("../actions/session_actions");
 const _nullErrors = [];
+
 const SignupErrorsReducer = (state = _nullErrors, action) =>{
     Object.freeze(state);
     switch(action.type){
         case RECEIVE_SIGNUP_ERRORS:
-            return action.signupErrors;
+            return action.errors;
         case RECEIVE_CURRENT_USER:
             return _nullErrors;
+        case REMOVE_SIGNUP_ERRORS:
+            return [];
         default:
             return state;
     }
 };
 
 export default SignupErrorsReducer;
-
-
 
