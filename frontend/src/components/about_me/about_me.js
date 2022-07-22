7e45a6ef297275381473928b92fa1333c7ceacee
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-07-21 20:03:48 -0400

    contributers rendering on bottom

--- frontend/src/components/about_me/about_me.js
+++ frontend/src/components/about_me/about_me.js
@@ -1,20 +1,61 @@
 import React from 'react';
 import "./../../styles/about_me.css";
+import linkedin from "./linkedin.png"
+import github from "./github.png"
+import porfolio from "./porfolio.png"
 
 class AboutMe extends React.Component {
 
     render() {
         return (
             <div className="about-me-container">
-                <div className="Alisher">
-                    Alisher Podavonov
-                    
+                <div className="person-container">
+                    <div className="person">
+                        Alisher Podavonov
+                    </div>
+                    <div className="about-me-links">
+                        <a href="https://github.com/apodavonov3616">
+                        <img src={github} alt="github" width = "39px" id="github"/>
+                        </a>
+                        <a href="https://alisher-podavonov.com/">
+                            <img src={porfolio} alt="portfolio" width = "39px" id="portfolio"/>
+                        </a>
+                        <a target="_blank" href="https://www.linkedin.com/in/alisher-podavonov-80b85a23b/">
+                            <img src={linkedin} alt="linkedin" width = "39px" id="linked"/>
+                        </a>
+                    </div>
                 </div>
-                <div className="Eric">
-                    Eric Balfour
+                <div className="person-container">
+                    <div className="person">
+                        Eric Balfour
+                    </div>
+                    <div className="about-me-links">
+                        <a href="google.com">
+                        <img src={github} alt="github" width = "39px" id="github"/>
+                        </a>
+                        <a href="google.com">
+                            <img src={porfolio} alt="portfolio" width = "39px" id="portfolio"/>
+                        </a>
+                        <a target="_blank" href="google.com">
+                            <img src={linkedin} alt="linkedin" width = "39px" id="linked"/>
+                        </a>
+                    </div>
                 </div>
-                <div className="David-Allen">
-                    David-Allen Asencio
+                <div className="person-container">
+                    <div className="person">
+                        David-Allen Asencio
+                    </div>
+                    <div className="about-me-links">
+                        <a href="https://github.com/davida11en">
+                        <img src={github} alt="github" width = "39px" id="github"/>
+                        </a>
+                        <a href="google.com">
+                            <img src={porfolio} alt="portfolio" width = "39px" id="portfolio"/>
+                        </a>
+                        <a target="_blank" href="https://www.linkedin.com/in/david-allen-asencio-9107b0122/">
+                            <img src={linkedin} alt="linkedin" width = "39px" id="linked"/>
+                        </a>
+                    </div>
                 </div>
             </div>
         );
