649fedfbd4394725ade28e1fc31c9673afe4c9fd
Eric Balfour <egbalfour@gmail.com>
2022-07-27 08:01:39 -0400

    updated db link

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://bcuatro:A6DdBZVo00GqPOd1@cluster0.gmlqmja.mongodb.net/?retryWrites=true&w=majority',
-    // secretOrKey: "secret12345678901234567890"
-    secretOrKey: "o38427FunF1Nd3r80q349ygp9rhADE"
+    mongoURI: 'mongodb+srv://bcuatro:jGLZtDAF2L1IJLPN@cluster0.gmlqmja.mongodb.net/?retryWrites=true&w=majority',
+    secretOrKey: "fnq349uFunF1Nd3rnfierw98ayr"
 }
